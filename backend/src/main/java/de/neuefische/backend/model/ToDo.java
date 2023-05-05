92df7175651bba34e643b9ba6a0c7f8e061d3582
Kristina <kristina.marienfeld@gmail.com>
2023-05-05 18:07:51 +0200

    add Integrationstests

--- backend/src/main/java/de/neuefische/backend/model/ToDo.java
+++ backend/src/main/java/de/neuefische/backend/model/ToDo.java
@@ -2,11 +2,13 @@ package de.neuefische.backend.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @AllArgsConstructor
 @Data
+@NoArgsConstructor
 public class ToDo {
 
     private String description;
---
+++ backend/src/test/java/de/neuefische/backend/controller/KanbanControllerTest.java
@@ -0,0 +1,161 @@
+package de.neuefische.backend.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.neuefische.backend.model.ToDo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+
+class KanbanControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @Test
+    @DirtiesContext
+    void getAllTodos() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/todo"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+
+    @Test
+    @DirtiesContext
+    void addNewTask() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/todo")
+                .contentType("application/json")
+                .content("""
+                { 
+                    "description": "Hallo", 
+                    "status":"OPEN"
+                }
+    """))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                { 
+                    "description": "Hallo", 
+                    "status":"OPEN"
+                }
+
+"""))
+                .andExpect(jsonPath("$.id").isNotEmpty());
+
+    }
+
+    @Test
+    @DirtiesContext
+    void getAllTodosList() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/board/todo"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+
+    @Test
+    @DirtiesContext
+    void getDetails() throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/todo")
+                        .contentType("application/json")
+                        .content("""
+                        { 
+                            "description": "Hallo", 
+                            "status":"OPEN"
+                        }
+                        """))
+                .andExpect(status().isOk())
+                .andReturn();
+
+
+        String content = result.getResponse().getContentAsString();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ToDo todo = objectMapper.readValue(content, ToDo.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/todo/"+todo.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                { 
+                    "description": "Hallo", 
+                    "status":"OPEN"
+                }
+                """));
+
+    }
+
+    @Test
+    @DirtiesContext
+    void editTodo_ThenReturn200OK_ReturnCorrectTask() throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/todo")
+                        .contentType("application/json")
+                        .content("""
+                        { 
+                            "description": "Hallo", 
+                            "status":"OPEN"
+                        }
+                        """))
+                        .andExpect(status().isOk())
+                        .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ToDo todo = objectMapper.readValue(content, ToDo.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/todo/"+todo.getId())
+                        .contentType("application/json")
+                        .content("""
+                        { 
+                            "description": "bye", 
+                            "status":"OPEN"
+                        }
+                        """))
+                        .andExpect(status().isOk())
+                        .andExpect(content().json("""
+                        [{ 
+                            "description": "bye", 
+                            "status":"OPEN"
+                        }]
+                                
+                                """));
+    }
+
+    @Test
+    void deleteTask() throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/api/todo")
+                        .contentType("application/json")
+                        .content("""
+                        { 
+                            "description": "Hallo", 
+                            "status":"OPEN"
+                        }
+                        """))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = result.getResponse().getContentAsString();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        ToDo todo = objectMapper.readValue(content, ToDo.class);
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/todo/"+todo.getId()))
+                    .andExpect(status().isOk())
+                    .andExpect(content().json("""
+                        []
+                                
+                                """));
+
+    }
+
+
+}
